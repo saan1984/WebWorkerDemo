e7748e1019f051af738b0e4405d6ae9dcc037a38
saan1984 <sandeep_giet@yahoo.com>
2015-04-06 21:33:17 +0530

    added an example on Error Handlerin Web Worker

--- sharedWorker/calculateLengthWorker.js
+++ sharedWorker/calculateLengthWorker.js
@@ -1,8 +1,8 @@
 self.addEventListener("connect", function (event) {
     var port = event.ports[0];
     port.addEventListener("message", function (event) {
-        var requstData = event.data,
-            inputString = requstData.string,
+        var requestData = event.data,
+            inputString = requestData.string,
             stringLength = inputString.length;
         port.postMessage({result:stringLength});
     }, false);
